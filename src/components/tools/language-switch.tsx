6e1b3072cfd43849805354e857df335212e670de
Guxxtavoww <gustavoaugustocar@hotmail.com>
2023-12-14 14:43:41 -0300

    Chore: contact form done

--- src/components/tools/language-switch.tsx
+++ src/components/tools/language-switch.tsx
@@ -11,7 +11,7 @@ export default function LanguageSwitch() {
 
   return (
     <button
-      className="fixed top-5 right-5 bg-white w-[3rem] h-[3rem] md:top-22 max-md:top-10 bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
+      className="fixed top-5 right-5 bg-white w-[3rem] h-[3rem] md:top-22 max-md:top-20 bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
       onClick={handleToggleTranslation}
       disabled={isLoadingTranslation}
       role="button"
