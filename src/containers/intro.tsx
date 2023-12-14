8cc93751f0c36a22b8dbf53f7c0d0f3f8f2effa7
Guxxtavoww <gustavoaugustocar@hotmail.com>
2023-12-14 15:31:29 -0300

    Chore: Adding both CVs

--- src/containers/intro.tsx
+++ src/containers/intro.tsx
@@ -102,7 +102,13 @@ export default function Intro() {
         </Link>
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href={
+            currentTranslation
+              ? currentTranslation.lang === 'pt-br'
+                ? '/GustavoAugusto-CV-PT-BR.pdf'
+                : '/GustavoAugusto-CV-Eng.pdf'
+              : '/GustavoAugusto-CV-PT-BR.pdf'
+          }
           download
         >
           Download CV{' '}
