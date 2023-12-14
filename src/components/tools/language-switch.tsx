2b5258588c18042c53d97f402e94e28ac3e8a6bf
Guxxtavoww <gustavoaugustocar@hotmail.com>
2023-12-14 12:11:10 -0300

    Chore: contact form done

--- src/components/tools/language-switch.tsx
+++ src/components/tools/language-switch.tsx
@@ -6,12 +6,16 @@ import { FaFlagUsa } from 'react-icons/fa';
 import { useTranslations } from '@/contexts/translation.context';
 
 export default function LanguageSwitch() {
-  const { handleToggleTranslation, currentTranslation } = useTranslations();
+  const { handleToggleTranslation, currentTranslation, isLoadingTranslation } =
+    useTranslations();
 
   return (
     <button
-      className="fixed top-5 right-5 bg-white w-[3rem] h-[3rem] max-md:top-22 bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
+      className="fixed top-5 right-5 bg-white w-[3rem] h-[3rem] md:top-22 max-md:top-10 bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
       onClick={handleToggleTranslation}
+      disabled={isLoadingTranslation}
+      role="button"
+      type="button"
     >
       {currentTranslation?.lang ? (
         currentTranslation.lang === 'pt-br' ? (
