4f3b766cc521500fc419ba4c5045e692bf316e9b
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-06-20 12:15:47 -0300

    Chore: adding projects

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
 import wmsBanner from './jpegs/wms-banner.jpeg';
 import eventsApp from './jpegs/events-app.jpeg';
+import expediaBanner from './pngs/expedia.png';
 import nextJsBlogBanner from './jpegs/next-js-blog-banner.jpeg';
 
-export { wmsBanner, nextJsBlogBanner, eventsApp };
+export { wmsBanner, nextJsBlogBanner, eventsApp, expediaBanner };
